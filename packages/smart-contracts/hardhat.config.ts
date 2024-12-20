f97faeb07e93d1d10f331cfee4e26c285020c0a2
panxiaoliang <1354383179@qq.com>
2024-12-20 11:08:14 +0800

    feat: optimization generate-contracts-metadata script

--- packages/smart-contracts/hardhat.config.ts
+++ packages/smart-contracts/hardhat.config.ts
@@ -1,5 +1,4 @@
 import { HardhatUserConfig } from "hardhat/config";
-import { parseEther } from "ethers";
 import "@nomicfoundation/hardhat-ignition-ethers";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-verify";
@@ -12,34 +11,6 @@ const config: HardhatUserConfig = {
   solidity: "0.8.27",
   defaultNetwork: "localhost",
   networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545",
-      chainId: 31337,
-      accounts: [
-        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
-        "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
-        "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",
-      ],
-    },
-    hardhat: {
-      accounts: [
-        {
-          privateKey:
-            "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
-          balance: parseEther("1000").toString(),
-        },
-        {
-          privateKey:
-            "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
-          balance: parseEther("1000").toString(),
-        },
-        {
-          privateKey:
-            "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",
-          balance: parseEther("1000").toString(),
-        },
-      ],
-    },
     // holesky: {
     //   url: "https://ethereum-holesky-rpc.publicnode.com",
     //   accounts: [process.env.PRIVATE_KEY!],
--- packages/smart-contracts/scripts/generate-contracts-metadata.ts
+++ packages/smart-contracts/scripts/generate-contracts-metadata.ts
@@ -1,8 +1,68 @@
 import chalk from "chalk";
 import { readFileSync, readdirSync, existsSync, writeFileSync } from "node:fs";
-import { join } from "node:path";
+import { dirname, join, relative, resolve } from "node:path";
 
 const getContractName = (path: string) => path.split(".json")[0];
+
+function getActualSourcesForContract(
+  sources: Record<string, any>,
+  contractName: string,
+  content: string
+) {
+  for (const sourcePath of Object.keys(sources)) {
+    const sourceName = sourcePath.split("/").pop()?.split(".sol")[0];
+    if (sourceName === contractName) {
+      const regex = /contract\s+(\w+)\s+is\s+([^{}]+)\{/;
+      const match = content.match(regex);
+
+      if (match) {
+        const inheritancePart = match[2];
+        // Split the inherited contracts by commas to get the list of inherited contracts
+        const inheritedContracts = inheritancePart
+          .split(",")
+          .map((contract) => `${contract.trim()}.sol`);
+
+        return inheritedContracts;
+      }
+      return [];
+    }
+  }
+  return [];
+}
+
+function getInheritedFunctions(
+  sources: Record<string, any>,
+  contractName: string,
+  content: string
+) {
+  const actualSources = getActualSourcesForContract(
+    sources,
+    contractName,
+    content
+  );
+  const inheritedFunctions = {} as Record<string, any>;
+
+  for (const sourceContractName of actualSources) {
+    const sourcePath = Object.keys(sources).find((key) =>
+      key.includes(`/${sourceContractName}`)
+    );
+    if (sourcePath) {
+      const sourceName = sourcePath?.split("/").pop()?.split(".sol")[0];
+      const { abi } = JSON.parse(
+        readFileSync(
+          join(process.cwd(), "artifacts", sourcePath, `${sourceName}.json`)
+        ).toString()
+      );
+      for (const functionAbi of abi) {
+        if (functionAbi.type === "function") {
+          inheritedFunctions[functionAbi.name] = sourcePath;
+        }
+      }
+    }
+  }
+
+  return inheritedFunctions;
+}
 
 const generateContractsMetadata = async () => {
   try {
@@ -32,12 +92,30 @@ const generateContractsMetadata = async () => {
       );
       const data = contracts.reduce<Record<string, any>>((acc, cur) => {
         const name = getContractName(cur);
-        if (!contractNames.includes(name)) {
-          contractNames.push(name);
+        const buildInfoPath = JSON.parse(
+          readFileSync(join(artifactsPath, `${name}.dbg.json`), "utf-8")
+        ).buildInfo.replace(/\\/g, "/");
+        const { abi, sourceName, contractName } = JSON.parse(
+          readFileSync(join(artifactsPath, cur), "utf8")
+        );
+        const buildInfo = JSON.parse(
+          readFileSync(resolve(artifactsPath, buildInfoPath), "utf8")
+        );
+        if (!contractNames.includes(contractName)) {
+          contractNames.push(contractName);
         }
-        acc[name] = {
+        const content = buildInfo.input.sources[sourceName].content;
+        const metadata =
+          buildInfo.output.contracts[sourceName][contractName].metadata;
+        const sources = metadata ? JSON.parse(metadata).sources ?? {} : {};
+        acc[contractName] = {
           address: contractAddresses[name],
-          abi: JSON.parse(readFileSync(join(artifactsPath, cur), "utf8")).abi,
+          abi,
+          inheritedFunctions: getInheritedFunctions(
+            sources,
+            contractName,
+            content
+          ),
         };
         return acc;
       }, {});
